1278d361100cf10a15386f272038727f5270cd78
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-05-16 17:16:46 -0300

    Add content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { MainChordsComponent } from './pages/main-chords/main-chords.component';
+import { HomeComponent } from './pages/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      { path: '', component: HomeComponent },
+      {path: 'page2', component: MainChordsComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/main-chords/main-chords.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-chords',
+  templateUrl: './main-chords.component.html',
+  styleUrls: ['./main-chords.component.scss']
+})
+export class MainChordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
